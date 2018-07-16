7d6ebc3fd15e0883da418536762f8a43c8c7441f
pkamath2 <git.pkamath2@gmail.com>
2018-07-16 17:23:22 +0800

    aesthetics

--- src/main/java/org/pk/web_analytics/properties/WebAnalyticsProducerProperties.java
+++ src/main/java/org/pk/web_analytics/properties/WebAnalyticsProducerProperties.java
@@ -1,8 +1,6 @@
 package org.pk.web_analytics.properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
